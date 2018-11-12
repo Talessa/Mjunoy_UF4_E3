ee7f49d8bcb0d2499c300a901bf2d402c6bcb2e3
Talessa <mjunoygomez@gmail.com>
2018-11-12 16:46:20 +0100

    completado el ejercicio uno daus

--- src/com/UF4_E3/Mjunoy/Ex1/Main.java
+++ src/com/UF4_E3/Mjunoy/Ex1/Main.java
@@ -1,8 +1,42 @@
 package com.UF4_E3.Mjunoy.Ex1;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Map<Integer,Integer> frequencia = new HashMap<>();
+        int d1;
+        int d2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Cuantas veces quieres lanzar los dados? ");
+       int lanzamientos = scanner.nextInt();
+
+        for(int i=0; i<lanzamientos; i++) {
+            int j = i+1;
+            System.out.print("Lanzamiento nº"+j+" ");
+            d1 = (int) (Math.random()*6) + 1;
+            System.out.print(d1);
+            d2 = (int) (Math.random()*6) + 1;
+            System.out.println(" + "+d2+"="+(d1+d2));
+            if(frequencia.containsKey(d1+d2)){
+                frequencia.put(d1+d2,frequencia.get(d1+d2)+1);
+            }
+            else{
+                frequencia.put(d1+d2,1);
+            }
+        }
+
+        System.out.println("La frecuencia de los lanzamientos a sido:");
+        System.out.println("| Numero | Frecuencia |");
+        for(Map.Entry<Integer,Integer> entry: frequencia.entrySet()) {
+            System.out.println("   "+entry.getKey() + "          " + entry.getValue());
+        }
+
 
     }
 }
